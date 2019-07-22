7f8ac6d38f45218b7d81ec8a8ad06c003cb8cb87
Soni suman <soni.suman@impelsys.com>
2019-07-22 10:55:51 +0530

    table view setup

---
+++ ToDoye/ToDoTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ToDoye
+//
+//  Created by Soni Suman on 7/19/19.
+//  Copyright © 2019 Soni Suman. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+  var itemArr = ["Buy Eggs", "Buy Vegitable","Buy milk"]
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+ //Mark: Tableview Datasource Delegates
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoTableCell", for: indexPath)
+    cell.textLabel?.text = itemArr[indexPath.row]
+    //cell.accessoryType = .checkmark
+    return cell
+  }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArr.count
+  }
+
+  //Mark: Tableview delegate
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     print(indexPath.row)
+     print(itemArr[indexPath.row])
+    if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark  {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    } else {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+
+}
+
+
+
+
